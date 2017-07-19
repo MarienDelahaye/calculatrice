e036584fbbc562d9bfceefa81cf8259cb2f21ea1
Delahaye Marien <delahaye.marien@gmail.com>
2017-07-19 10:07:42 +0200

    Started to work on my main.js and edited index.html & main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 // Variables
-var buttons = document.getElementsByClassName("buttons");
+var display = document.getElementById("header");
+
 // Functions
-function Calculator()
+function removeValue(){
+    document.getElementById("resultat").value = "";
+}
+function neuf(){
+    var neuf = document.getElementById("neuf").value;
+	var value = document.getElementById("resultat").value;
+	document.getElementById("resultat").value = neuf + value;
+}
+function huit(){
+    var huit = document.getElementById("huit").value;
+    var value = document.getElementById("resultat").value;
+    document.getElementById("resultat").value = huit + value;
 
-
+}
+function sept(){
+    var sept = document.getElementById("sept").value;
+    var value = document.getElementById("resultat").value;
+    document.getElementById("resultat").value = sept + value;
+}
+function six(){
+    var six = document.getElementById("six").value;
+    var value = document.getElementById("resultat").value;
+    document.getElementById("resultat").value = six + value;
+}
 
 // Script Part
